0ba4b94e50eb4bfdeab48fef7fe5ce83139e6ec0
Shura Knows <shuraknows@gmail.com>
2024-08-18 09:51:15 +0300

    functions

--- 4_functions/src/main.rs
+++ 4_functions/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    // declared via fn
+    // functions should have snake_case names
+    another_function();
+    parametrized_function(123);
+    multiple_params_function("foo", 123);
+}
+
+fn multiple_params_function(key: &str, value: i32) {
+    println!("[{key}] => {value}");
+}
+
+fn parametrized_function(x: i32) {
+    println!("The value of x is: {}", x);
+}
+
+fn another_function() {
+    println!("Another function.");
 }
