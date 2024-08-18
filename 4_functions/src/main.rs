1a74e066035c22fc91a701f9da3011565af5674e
Shura Knows <shuraknows@gmail.com>
2024-08-18 10:24:06 +0300

    functions

--- 4_functions/src/main.rs
+++ 4_functions/src/main.rs
@@ -4,6 +4,51 @@ fn main() {
     another_function();
     parametrized_function(123);
     multiple_params_function("foo", 123);
+
+
+    // #################################################
+    // ########## Statements vs Expressions ############
+    // #################################################
+
+    // statements - instructions that perform some action and do not return a value
+    // expressions - evaluate to a resulting value
+
+    // Statements
+    let _y = 6; // statement
+    // function definitions are also statements
+    // In Rust unlike other languages assignment does not return a value
+
+    // Expressions
+    // Expressions evaluate to a resulting value
+
+    let xyz = {
+        let xyz = 3;
+        xyz + 1 // this is an expression  WITHOUT SEMICOLON!!
+       // xyz // even this is an expression
+    };
+
+    println!("The value of y is: {xyz}");
+
+    // !!!!!!!!!!!!
+    // Expressions do not include ending semicolons.
+    // If you add a semicolon to the end of an expression, you turn it into a statement,
+    // and it will then not return a value.
+
+
+    // #################################################
+    // ########## Functions with return values #########
+    // #################################################
+    let xxx = five();
+    println!("The value of xxx is: {}", xxx);
+    println!("The value of xxx is: {}", plus_five(3));
+}
+
+fn plus_five(value: i32) -> i32 {
+    value + 5
+}
+
+fn five() -> i32 {
+    5
 }
 
 fn multiple_params_function(key: &str, value: i32) {
